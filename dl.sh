16f56fe2928c7a7d15390827ac96e856faab3f0c
Kyle <2584270+kylemsguy@users.noreply.github.com>
2024-02-13 01:25:43 -0800

    Update download command to use cookies from the browser

--- dl.sh
+++ dl.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 channelname="Rosemi_Lovelock"
 outputfilename='%(upload_date)s-%(title)s-%(id)s.%(ext)s'
-cookies="~/videos/cookies.txt"
-# yt-dlp --cookies "$cookies" --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "members.txt" "https://www.youtube.com/@${channelname}/membership" -o "members/$outputfilename"
-yt-dlp -a membersurls.txt --cookies "$cookies" --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "members.txt" "https://www.youtube.com/@${channelname}/membership" -o "members/$outputfilename"
-yt-dlp --cookies "$cookies" --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "streams.txt" "https://www.youtube.com/@${channelname}/streams" -o "streams/$outputfilename"
-yt-dlp --cookies "$cookies" --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "shorts.txt" "https://www.youtube.com/@${channelname}/shorts" -o "shorts/$outputfilename"
-yt-dlp --cookies "$cookies" --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "videos.txt" "https://www.youtube.com/@${channelname}/videos" -o "videos/$outputfilename"
+
+# To download members data, make sure you have all the member video URLs in membersurls.txt or change this filename
+memberurlsfile="membersurls.txt"
+
+yt-dlp -a "$membersurlsfile" --cookies-from-browser firefox --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "members.txt" "https://www.youtube.com/@${channelname}/membership" -o "members/$outputfilename"
+yt-dlp --cookies-from-browser firefox --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "streams.txt" "https://www.youtube.com/@${channelname}/streams" -o "streams/$outputfilename"
+yt-dlp --cookies-from-browser firefox --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "shorts.txt" "https://www.youtube.com/@${channelname}/shorts" -o "shorts/$outputfilename"
+yt-dlp --cookies-from-browser firefox --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "videos.txt" "https://www.youtube.com/@${channelname}/videos" -o "videos/$outputfilename"
+
+# Old command for downloading members content only (no longer works due to YT changes)
+# yt-dlp --cookies-from-browser firefox --no-abort-on-error --sub-langs all --write-subs --embed-subs --add-metadata --write-description --write-thumbnail --embed-thumbnail --embed-metadata --embed-info-json --write-info-json --match-filter !is_live --download-archive "members.txt" "https://www.youtube.com/@${channelname}/membership" -o "members/$outputfilename"
